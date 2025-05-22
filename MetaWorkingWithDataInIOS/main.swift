40ebcb51b7b02d5a621cbc0cf0208872db8dc129
luu-9798 <trungluu9798@gmail.com>
2025-05-22 11:08:17 +0700

    Initialize a new URL object with the Little Lemon address String

--- MetaWorkingWithDataInIOS/main.swift
+++ MetaWorkingWithDataInIOS/main.swift
@@ -10,6 +10,9 @@ import Foundation
 func HTTP_request_and_response_in_Swift() {
     //Define the Little Lemon server address as a constant String
     let littleLemonAddress = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonSimpleMenu.json"
+    
+    //Initialize a new URL object with the Little Lemon address String
+    let url = URL(string: littleLemonAddress)
 }
 
 HTTP_request_and_response_in_Swift()
