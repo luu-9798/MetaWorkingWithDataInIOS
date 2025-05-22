27fa4a8d92afc671221d88945a6f0bbc5a8a9767
luu-9798 <trungluu9798@gmail.com>
2025-05-22 11:11:23 +0700

    Use guard to unwrap the optional url constant

--- MetaWorkingWithDataInIOS/main.swift
+++ MetaWorkingWithDataInIOS/main.swift
@@ -7,12 +7,17 @@
 
 import Foundation
 
-func HTTP_request_and_response_in_Swift() {
+func HTTP_request_and_response_in_Swift() throws {
     //Define the Little Lemon server address as a constant String
     let littleLemonAddress = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonSimpleMenu.json"
     
     //Initialize a new URL object with the Little Lemon address String
     let url = URL(string: littleLemonAddress)
+    
+    //Use guard to unwrap the optional url constant
+    guard let url = url else {
+        throw NSError()
+    }
 }
 
-HTTP_request_and_response_in_Swift()
+try HTTP_request_and_response_in_Swift()
