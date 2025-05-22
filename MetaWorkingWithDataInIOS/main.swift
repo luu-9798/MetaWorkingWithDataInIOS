ae3df15586e1e9729a455bb4150f66f7e06875c6
luu-9798 <trungluu9798@gmail.com>
2025-05-22 11:06:36 +0700

    Define the Little Lemon server address as a constant String

--- MetaWorkingWithDataInIOS/main.swift
+++ MetaWorkingWithDataInIOS/main.swift
@@ -7,5 +7,9 @@
 
 import Foundation
 
-print("Hello, World!")
+func HTTP_request_and_response_in_Swift() {
+    //Define the Little Lemon server address as a constant String
+    let littleLemonAddress = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonSimpleMenu.json"
+}
 
+HTTP_request_and_response_in_Swift()
